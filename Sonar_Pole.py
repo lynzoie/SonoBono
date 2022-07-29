326b3c304ad408fb1c8fb76e9a91efa2a4e24adf
Echo Lab <echolab@pop-os.localdomain>
2022-07-29 10:42:40 -0400

    removed repeat test 5 times

--- Sonar_Pole.py
+++ Sonar_Pole.py
@@ -46,45 +46,44 @@ if __name__ == '__main__':
     ang_inc = 2         # degrees
 
     dist = np.arange(start_dist, final_dist+dist_inc, dist_inc)
-    for r in range(5): # run the test 5 times
-        for x in dist:
-            temp_l = np.zeros((181,7000,5))
-            temp_r = np.zeros((181,7000,5))
-            
-            for a in range(0, 181, ang_inc):
-                # Modifying coordinates for run
-                y = 0
-                temp_a = a  # before modifying angle, make sure we store temp data in right index of temp_l/temp_r
-                a += 90
-                a = wrap_to_180(a)
+    for x in dist:
+        temp_l = np.zeros((181,7000,5))
+        temp_r = np.zeros((181,7000,5))
+        
+        for a in range(0, 181, ang_inc):
+            # Modifying coordinates for run
+            y = 0
+            temp_a = a  # before modifying angle, make sure we store temp data in right index of temp_l/temp_r
+            a += 90
+            a = wrap_to_180(a)
 
-                # travel code
-                gps_robot.get_robot_pose(c)
-                if abs(wrap_to_180(gps_robot.theta - a)) >= 3:
-                    gps_robot.travel(robot, c, x, y, a)
-                else:
-                    move_robot.turn(a=ang_inc, bot=robot, smooth_stop=False, w_error=3.416)
+            # travel code
+            gps_robot.get_robot_pose(c)
+            if abs(wrap_to_180(gps_robot.theta - a)) >= 3:
+                gps_robot.travel(robot, c, x, y, a)
+            else:
+                move_robot.turn(a=ang_inc, bot=robot, smooth_stop=False, w_error=3.416)
 
-                # echo code
-                left5 = np.zeros((7000,5))
-                right5 = np.zeros((7000,5))
+            # echo code
+            left5 = np.zeros((7000,5))
+            right5 = np.zeros((7000,5))
 
-                # grab 5 measurements of echo data in the current pose
-                for k in range(5):
-                    observe.one_echo()
-                    left5[:,k] = observe.echo['left']
-                    right5[:,k] = observe.echo['right']
+            # grab 5 measurements of echo data in the current pose
+            for k in range(5):
+                observe.one_echo()
+                left5[:,k] = observe.echo['left']
+                right5[:,k] = observe.echo['right']
 
-                temp_l[int(temp_a)] = left5
-                temp_r[int(temp_a)] = right5
+            temp_l[int(temp_a)] = left5
+            temp_r[int(temp_a)] = right5
 
-            # save trial_dist
-            path = '/home/mendel/SonoBono/echo_data/'
-            file_name = str(r) + "_" + str(x) + ".npz"
-            np.savez( os.path.join(path, file_name), left_data = temp_l, right_data = temp_r)
-            print("Saved ", file_name, "to path ", path)
-            garbage = [temp_l, temp_r, left5, right5]
-            del temp_l, temp_r, left5, right5
-            del garbage
+        # save trial_dist
+        path = '/home/mendel/SonoBono/echo_data/'
+        file_name = str(x) + ".npz"
+        np.savez( os.path.join(path, file_name), left_data = temp_l, right_data = temp_r)
+        print("Saved ", file_name, "to path ", path)
+        garbage = [temp_l, temp_r, left5, right5]
+        del temp_l, temp_r, left5, right5
+        del garbage
 
-    robot.drive_stop()
+    robot.stop()
